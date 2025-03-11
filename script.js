9f8db92cc46b0caacf9b04f7595627851c97b236
duzynn <145466909+duzynn@users.noreply.github.com>
2025-03-11 16:04:33 -0300

    11/03

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function()) {
+document.addEventListener('DOMContentLoaded', function() {
+    const aumentaFonteBotao=document.getElementsById('aumentar-fonte');
 
-
-}
+    let  tamanhoAtualFonte=1;
+    aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtual +=;
+    document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+    })
+})
