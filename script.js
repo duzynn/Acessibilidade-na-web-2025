914f3d6c2e67162db4c787eece898c90dea0dd6c
duzynn <145466909+duzynn@users.noreply.github.com>
2025-03-25 13:41:14 -0300

    25

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.style.fontSize=`${tamanhoAtualFonte}rem`
     })
     diminuirFonteBotao.addEventListener('click', function(){
-tamanhoAtualFonte -= 0.1;
-document.body.style.fontSize=`$(tamanhoAtualFonte)rem`
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize=`$(tamanhoAtualFonte)rem`
 
     })
 })
